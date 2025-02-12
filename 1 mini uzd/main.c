25643b9d90f17776ac93fe5901dfffc8532367f5
Robertas <robervicsat@gmail.com>
2025-02-12 14:17:56 +0200

    m

---
+++ 1 mini uzd/main.c
@@ -0,0 +1,18 @@
+// function mySum
+// precondition: a and b are integers
+// postcondition: returns the sum of a and b
+int mySum(int a, int b) {
+    return a + b;
+}
+
+// function myDivision
+// precondition: a and b are integers
+// postcondition: 0 a and b if b != 0, otherwise returns -1
+void myDivision(int a, int b, int *result, int *remainder) {
+    if (b == 0) {
+        return -1;
+    }
+    *result = a / b;
+    *remainder = a % b;
+    return 0;
+}
