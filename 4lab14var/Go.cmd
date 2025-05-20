0acd843a823ffa4c0e03b81b1e81a3ee7db0754a
Robertas <robervicsat@gmail.com>
2025-05-20 18:07:29 +0300

    go.cmd modified

--- 4lab14var/Go.cmd
+++ 4lab14var/Go.cmd
@@ -6,10 +6,50 @@ mingw32-make.exe
 goto :toliau
 
 :savasPC
+echo 7 > input1.txt
+echo 0 1 >> input1.txt
+echo 0 2 >> input1.txt
+echo 1 3 >> input1.txt
+echo 1 4 >> input1.txt
+echo 2 5 >> input1.txt
+echo 2 6 >> input1.txt
+
+echo 8 > input2.txt
+echo 0 1 >> input2.txt
+echo 0 2 >> input2.txt
+echo 1 3 >> input2.txt
+echo 1 4 >> input2.txt
+echo 2 5 >> input2.txt
+echo 2 6 >> input2.txt
+echo 3 7 >> input2.txt
+echo 4 7 >> input2.txt
+echo 5 7 >> input2.txt
+echo 6 7 >> input2.txt
+
 make
 main.exe < input1.txt > output1.txt
 main.exe input2.txt > output2.txt
 
 :toliau
+echo 7 > input1.txt
+echo 0 1 >> input1.txt
+echo 0 2 >> input1.txt
+echo 1 3 >> input1.txt
+echo 1 4 >> input1.txt
+echo 2 5 >> input1.txt
+echo 2 6 >> input1.txt
+
+echo 8 > input2.txt
+echo 0 1 >> input2.txt
+echo 0 2 >> input2.txt
+echo 1 3 >> input2.txt
+echo 1 4 >> input2.txt
+echo 2 5 >> input2.txt
+echo 2 6 >> input2.txt
+echo 3 7 >> input2.txt
+echo 4 7 >> input2.txt
+echo 5 7 >> input2.txt
+echo 6 7 >> input2.txt
+
 main.exe < input1.txt > output1.txt
 main.exe input2.txt > output2.txt
